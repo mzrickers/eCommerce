352e97b6628bbb3bff8a3940eedcc6e0a905e0f2
Zach Rickers <mzrickers@gmail.com>
2022-01-26 17:59:41 +0100

    added users.js

---
+++ repositories/users.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+
+  async getAll() {
+    // Open the file called this.file
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, 
+        {encoding: 'utf8'
+      })
+    );
+  }
+}
+
+
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+
+  const users = await repo.getAll();
+
+  console.log(users);
+}
+
+test();
