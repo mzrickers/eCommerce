746b3800620b7a0f958e04ebf6365fe01f590c57
Zach Rickers <mzrickers@gmail.com>
2022-02-23 11:04:35 +0100

    added layout template

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,20 +1,17 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-      </head>
-      <body>
-        <div>
-          Your id is: ${req.session.userId}
-          <form method="POST">
-            <input name='email' placeholder='email' />
-            <input name='password' placeholder='password' />
-            <input name='passwordConfirmation' placeholder='password confirmation' />
-            <button>Sign Up</button>
-          </form>
-        </div>
-      </body>
-    </html>
-  `;
+  return layout({
+    content: `
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name='email' placeholder='email' />
+        <input name='password' placeholder='password' />
+        <input name='passwordConfirmation' placeholder='password confirmation' />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `
+  });
 };
