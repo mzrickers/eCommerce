4899db344f39145c9c0c14362e71e2e2a37b6ac8
Zach Rickers <mzrickers@gmail.com>
2022-02-23 10:54:04 +0100

    completed html body

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,20 @@
 module.exports = ({ req }) => {
   return `
-    <div>
-      Your id is: ${req.session.userId}
-      <form method="POST">
-        <input name='email' placeholder='email' />
-        <input name='password' placeholder='password' />
-        <input name='passwordConfirmation' placeholder='password confirmation' />
-        <button>Sign Up</button>
-      </form>
-    </div>
+    <!DOCTYPE html>
+    <html>
+      <head>
+      </head>
+      <body>
+        <div>
+          Your id is: ${req.session.userId}
+          <form method="POST">
+            <input name='email' placeholder='email' />
+            <input name='password' placeholder='password' />
+            <input name='passwordConfirmation' placeholder='password confirmation' />
+            <button>Sign Up</button>
+          </form>
+        </div>
+      </body>
+    </html>
   `;
 };
